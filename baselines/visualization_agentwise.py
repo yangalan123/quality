484263ccac19069d679983514585f519b3b5b288
Chenghao Yang <chenghao.yang@columbia.edu>
2023-01-12 19:53:28 -0600

    update from mac

---
+++ baselines/visualization_agentwise.py
@@ -0,0 +1,8 @@
+import json
+import os
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    model_path = "race_deberta_large_epoch_20"
+    exp_dir = "dpr_agent_dpr_sum_300"
+    filepath = "/data/chenghao/quality/baselines/experiment/"
